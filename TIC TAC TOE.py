e4fc33dbcff461099a467425647b2dee9de7cbe9
GovindKurapati <50044672+GovindKurapati@users.noreply.github.com>
2020-05-22 21:59:48 +0530

    Add files via upload
    initial commit

---
+++ TIC TAC TOE.py
@@ -0,0 +1,122 @@
+
+def display(board):
+    print(board[1]+'|'+board[2]+'|'+board[3])
+    print("-----")
+    print(board[4]+'|'+board[5]+'|'+board[6])
+    print("-----")
+    print(board[7]+'|'+board[8]+'|'+board[9])
+
+def input_user():
+    player1=""
+    while player1!='X' and player1!='O':
+        player1=input("Player1 Enter your choice X or O:").upper()
+    if player1=='X':
+        player2='O'
+    else:
+        player2='X'
+    return (player1,player2)
+
+def win_check(board,mark):
+    
+    return ((board[7] == mark and board[8] == mark and board[9] == mark) or # across the top
+    (board[4] == mark and board[5] == mark and board[6] == mark) or # across the middle
+    (board[1] == mark and board[2] == mark and board[3] == mark) or # across the bottom
+    (board[7] == mark and board[4] == mark and board[1] == mark) or # down the middle
+    (board[8] == mark and board[5] == mark and board[2] == mark) or # down the middle
+    (board[9] == mark and board[6] == mark and board[3] == mark) or # down the right side
+    (board[7] == mark and board[5] == mark and board[3] == mark) or # diagonal
+    (board[9] == mark and board[5] == mark and board[1] == mark)) # diagonal
+
+def place_marker(board,marker,position):
+    board[position]=marker
+
+import random
+
+def player_choose():
+    
+    flip=random.randint(0,1)
+    
+    if flip==0:
+        return 'Player1'
+    else:
+        return 'Player2'
+
+
+def free_position(board,position):
+    return board[position]==" "
+
+def full_board(board):
+    
+    for i in range(1,10):
+        if free_position(board,i):
+            return False
+        
+    return True
+
+def player_choice(board):
+    
+    position=0
+    while position not in [1,2,3,4,5,6,7,8,9] or not free_position(board,position):
+        position=int(input('Enter your position (1-9) '))
+    return position
+
+def replay():
+    
+    answer=input("Do you want to play again?? Yes or No").lower()
+    
+    return answer=="yes"
+
+print('Welcome to TIC TAC TOE')
+while True:
+    
+    the_board=[" "]*10
+    player1_marker,player2_marker=input_user()
+    turn=player_choose()
+    print(turn + ' will play first')
+    print()
+    play_game=input('Ready to play? yes or no ?')
+    
+    if play_game=='yes':
+        game_on=True
+        
+    else:
+        game_on=False
+    
+    while game_on:
+        if turn=='Player1':
+            display(the_board)
+            position=player_choice(the_board)
+            place_marker(the_board,player1_marker,position)
+            
+            if win_check(the_board,player1_marker):
+                display(the_board)
+                print('!!Player1 HAS WON!!')
+                game_on=False
+            else:
+                if full_board(the_board):
+                    display(the_board)
+                    print('TIE GAME')
+                    game_on=False
+                else:
+                    turn="Player2"
+                    
+        else:
+            display(the_board)
+            position=player_choice(the_board)
+            place_marker(the_board,player2_marker,position)
+            
+            if win_check(the_board,player2_marker):
+                display(the_board)
+                print('!!Player2 HAS WON!!')
+                game_on=False
+            else:
+                if full_board(the_board):
+                    display(the_board)
+                    print('TIE GAME')
+                    game_on=False
+                else:
+                    turn="Player1"
+            
+            
+    if not replay():
+        break
